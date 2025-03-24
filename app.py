b40497aca0047a7d0d414745e125dd54fb1defe8
FERXODJ <joseduvelyhuiza@gmail.com>
2025-03-24 08:09:35 -0400

    inicar flask en vercel

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#rutas
+@app.route('/')
+def raiz():
+    return render_template('inicio.html')
+
+#ruta paa nosotros
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+#Bloque de Prueba
+if __name__ == '__main__':
+    app.run(debug=True)
